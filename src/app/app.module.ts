9079c2acab21422f5f5783a46d95f4561a728539
Toty-01 <caucheanthony@gmail.com>
2023-04-26 14:22:13 +0200

    restart project

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { SingleMovieComponent } from './movie-list/single-movie/single-movie.component';
+import { MovieFormComponent } from './movie-list/movie-form/movie-form.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    MovieListComponent,
+    SingleMovieComponent,
+    MovieFormComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
